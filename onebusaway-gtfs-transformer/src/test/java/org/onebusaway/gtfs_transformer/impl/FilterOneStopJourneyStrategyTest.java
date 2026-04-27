6fe82444e626bbdd89aceef001bf28618d610804
afreche <alexandre.freche@lumiplan.com>
2026-04-27 18:23:04 +0200

    chore(): format sources

--- onebusaway-gtfs-transformer/src/test/java/org/onebusaway/gtfs_transformer/impl/FilterOneStopJourneyStrategyTest.java
+++ onebusaway-gtfs-transformer/src/test/java/org/onebusaway/gtfs_transformer/impl/FilterOneStopJourneyStrategyTest.java
@@ -1,93 +1,92 @@
 package org.onebusaway.gtfs_transformer.impl;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.onebusaway.gtfs.impl.GtfsRelationalDaoImpl;
 import org.onebusaway.gtfs.model.*;
 import org.onebusaway.gtfs_transformer.services.TransformContext;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 class FilterOneStopJourneyStrategyTest {
 
-    private final FilterOneStopJourneyStrategy tested = new FilterOneStopJourneyStrategy();
+  private final FilterOneStopJourneyStrategy tested = new FilterOneStopJourneyStrategy();
 
-    @Test
-    void runFilterOneStopJourneyTest() {
-        GtfsRelationalDaoImpl dao = new GtfsRelationalDaoImpl();
-        TransformContext context = new TransformContext();
+  @Test
+  void runFilterOneStopJourneyTest() {
+    GtfsRelationalDaoImpl dao = new GtfsRelationalDaoImpl();
+    TransformContext context = new TransformContext();
 
-        Trip emptyTrip = new Trip();
-        AgencyAndId id = new AgencyAndId();
-        id.setId("empty");
-        emptyTrip.setId(id);
-        dao.saveEntity(emptyTrip);
+    Trip emptyTrip = new Trip();
+    AgencyAndId id = new AgencyAndId();
+    id.setId("empty");
+    emptyTrip.setId(id);
+    dao.saveEntity(emptyTrip);
 
-        Trip oneStopTrip = new Trip();
-        AgencyAndId idOneStop = new AgencyAndId();
-        idOneStop.setId("oneStop");
-        oneStopTrip.setId(idOneStop);
-        dao.saveEntity(oneStopTrip);
+    Trip oneStopTrip = new Trip();
+    AgencyAndId idOneStop = new AgencyAndId();
+    idOneStop.setId("oneStop");
+    oneStopTrip.setId(idOneStop);
+    dao.saveEntity(oneStopTrip);
 
-        Trip validTrip = new Trip();
-        AgencyAndId validTripId = new AgencyAndId();
-        validTripId.setId("valid");
-        validTrip.setId(validTripId);
-        dao.saveEntity(validTrip);
+    Trip validTrip = new Trip();
+    AgencyAndId validTripId = new AgencyAndId();
+    validTripId.setId("valid");
+    validTrip.setId(validTripId);
+    dao.saveEntity(validTrip);
 
-        StopTime stopTimeToRemove = new StopTime();
-        stopTimeToRemove.setTrip(oneStopTrip);
-        dao.saveEntity(stopTimeToRemove);
+    StopTime stopTimeToRemove = new StopTime();
+    stopTimeToRemove.setTrip(oneStopTrip);
+    dao.saveEntity(stopTimeToRemove);
 
-        StopTime stopTimeToKeep = new StopTime();
-        stopTimeToKeep.setTrip(validTrip);
-        dao.saveEntity(stopTimeToKeep);
+    StopTime stopTimeToKeep = new StopTime();
+    stopTimeToKeep.setTrip(validTrip);
+    dao.saveEntity(stopTimeToKeep);
 
-        StopTime stopTimeToKeep2 = new StopTime();
-        stopTimeToKeep2.setTrip(validTrip);
-        dao.saveEntity(stopTimeToKeep2);
+    StopTime stopTimeToKeep2 = new StopTime();
+    stopTimeToKeep2.setTrip(validTrip);
+    dao.saveEntity(stopTimeToKeep2);
 
-        Frequency frequencyToRemove = new Frequency();
-        frequencyToRemove.setTrip(emptyTrip);
-        dao.saveEntity(frequencyToRemove);
+    Frequency frequencyToRemove = new Frequency();
+    frequencyToRemove.setTrip(emptyTrip);
+    dao.saveEntity(frequencyToRemove);
 
-        Frequency frequencyToRemove2 = new Frequency();
-        frequencyToRemove2.setTrip(oneStopTrip);
-        dao.saveEntity(frequencyToRemove2);
+    Frequency frequencyToRemove2 = new Frequency();
+    frequencyToRemove2.setTrip(oneStopTrip);
+    dao.saveEntity(frequencyToRemove2);
 
-        Frequency frequencyToKeep = new Frequency();
-        frequencyToKeep.setTrip(validTrip);
-        dao.saveEntity(frequencyToKeep);
+    Frequency frequencyToKeep = new Frequency();
+    frequencyToKeep.setTrip(validTrip);
+    dao.saveEntity(frequencyToKeep);
 
-        Transfer transferToRemove = new Transfer();
-        transferToRemove.setFromTrip(emptyTrip);
-        transferToRemove.setToTrip(validTrip);
-        dao.saveEntity(transferToRemove);
+    Transfer transferToRemove = new Transfer();
+    transferToRemove.setFromTrip(emptyTrip);
+    transferToRemove.setToTrip(validTrip);
+    dao.saveEntity(transferToRemove);
 
-        Transfer transferToRemove2 = new Transfer();
-        transferToRemove2.setToTrip(oneStopTrip);
-        transferToRemove2.setFromTrip(validTrip);
-        dao.saveEntity(transferToRemove2);
+    Transfer transferToRemove2 = new Transfer();
+    transferToRemove2.setToTrip(oneStopTrip);
+    transferToRemove2.setFromTrip(validTrip);
+    dao.saveEntity(transferToRemove2);
 
-        Transfer transferToKeep = new Transfer();
-        transferToKeep.setToTrip(validTrip);
-        transferToKeep.setFromTrip(validTrip);
-        dao.saveEntity(transferToKeep);
+    Transfer transferToKeep = new Transfer();
+    transferToKeep.setToTrip(validTrip);
+    transferToKeep.setFromTrip(validTrip);
+    dao.saveEntity(transferToKeep);
 
-        tested.run(context, dao);
+    tested.run(context, dao);
 
-        assertEquals(1, dao.getAllTrips().size());
-        assertTrue(dao.getAllTrips().contains(validTrip));
+    assertEquals(1, dao.getAllTrips().size());
+    assertTrue(dao.getAllTrips().contains(validTrip));
 
-        assertEquals(2, dao.getAllStopTimes().size());
-        assertTrue(dao.getAllStopTimes().contains(stopTimeToKeep));
-        assertTrue(dao.getAllStopTimes().contains(stopTimeToKeep2));
+    assertEquals(2, dao.getAllStopTimes().size());
+    assertTrue(dao.getAllStopTimes().contains(stopTimeToKeep));
+    assertTrue(dao.getAllStopTimes().contains(stopTimeToKeep2));
 
-        assertEquals(1, dao.getAllFrequencies().size());
-        assertTrue(dao.getAllFrequencies().contains(frequencyToKeep));
+    assertEquals(1, dao.getAllFrequencies().size());
+    assertTrue(dao.getAllFrequencies().contains(frequencyToKeep));
 
-        assertEquals(1, dao.getAllTransfers().size());
-        assertTrue(dao.getAllTransfers().contains(transferToKeep));
-    }
-
+    assertEquals(1, dao.getAllTransfers().size());
+    assertTrue(dao.getAllTransfers().contains(transferToKeep));
+  }
 }
--- onebusaway-gtfs-transformer/src/test/java/org/onebusaway/gtfs_transformer/impl/NonStandardStopTransformerStrategyTest.java
+++ onebusaway-gtfs-transformer/src/test/java/org/onebusaway/gtfs_transformer/impl/NonStandardStopTransformerStrategyTest.java
@@ -1,53 +1,53 @@
 package org.onebusaway.gtfs_transformer.impl;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Test;
 import org.onebusaway.gtfs.model.Route;
 import org.onebusaway.gtfs.model.Stop;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class NonStandardStopTransformerStrategyTest {
 
-    private static final String DEFAULT_NAME = "DEFAULT_NAME";
-    private static final double DEFAULT_LATITUDE = 43.481402;
-    private static final double DEFAULT_LONGITUDE = -1.514699;
-    private static final double DELTA = 0.001;
+  private static final String DEFAULT_NAME = "DEFAULT_NAME";
+  private static final double DEFAULT_LATITUDE = 43.481402;
+  private static final double DEFAULT_LONGITUDE = -1.514699;
+  private static final double DELTA = 0.001;
 
-    private final NonStandardStopTransformerStrategy tested =
-            new NonStandardStopTransformerStrategy(DEFAULT_NAME, DEFAULT_LATITUDE, DEFAULT_LONGITUDE);
+  private final NonStandardStopTransformerStrategy tested =
+      new NonStandardStopTransformerStrategy(DEFAULT_NAME, DEFAULT_LATITUDE, DEFAULT_LONGITUDE);
 
-    @Test
-    void shouldSetDefaultValueForStopIfNotSetTest() {
-        Stop stop = new Stop();
+  @Test
+  void shouldSetDefaultValueForStopIfNotSetTest() {
+    Stop stop = new Stop();
 
-        tested.handleEntity(stop);
+    tested.handleEntity(stop);
 
-        assertEquals(DEFAULT_NAME, stop.getName());
-        assertEquals(DEFAULT_LATITUDE, stop.getLat(), DELTA);
-        assertEquals(DEFAULT_LONGITUDE, stop.getLon(), DELTA);
-    }
+    assertEquals(DEFAULT_NAME, stop.getName());
+    assertEquals(DEFAULT_LATITUDE, stop.getLat(), DELTA);
+    assertEquals(DEFAULT_LONGITUDE, stop.getLon(), DELTA);
+  }
 
-    @Test
-    void shouldNotSetDefaultValueForStopIfValueExistsTest() {
-        Stop stop = new Stop();
-        stop.setName("NAME");
-        stop.setLat(48);
-        stop.setLon(-1);
+  @Test
+  void shouldNotSetDefaultValueForStopIfValueExistsTest() {
+    Stop stop = new Stop();
+    stop.setName("NAME");
+    stop.setLat(48);
+    stop.setLon(-1);
 
-        tested.handleEntity(stop);
+    tested.handleEntity(stop);
 
-        assertNotEquals(DEFAULT_NAME, stop.getName());
-        assertNotEquals(DEFAULT_LATITUDE, stop.getLat(), DELTA);
-        assertNotEquals(DEFAULT_LONGITUDE, stop.getLon(), DELTA);
-    }
+    assertNotEquals(DEFAULT_NAME, stop.getName());
+    assertNotEquals(DEFAULT_LATITUDE, stop.getLat(), DELTA);
+    assertNotEquals(DEFAULT_LONGITUDE, stop.getLon(), DELTA);
+  }
 
-    @Test
-    void shouldNotMakeAnyTransformationForOtherTypesTest() {
-        Route route = new Route();
+  @Test
+  void shouldNotMakeAnyTransformationForOtherTypesTest() {
+    Route route = new Route();
 
-        tested.handleEntity(route);
+    tested.handleEntity(route);
 
-        assertNull(route.getShortName());
-        assertNull(route.getLongName());
-    }
+    assertNull(route.getShortName());
+    assertNull(route.getLongName());
+  }
 }
