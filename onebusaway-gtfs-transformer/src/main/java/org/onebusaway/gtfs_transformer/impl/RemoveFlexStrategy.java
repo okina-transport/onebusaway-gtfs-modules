2b26519418d74fd58048971d19ef9294a2ef87bc
afreche <alexandre.freche@lumiplan.com>
2026-04-27 14:59:53 +0200

    fix(): ConcurrentModificationException

--- onebusaway-gtfs-transformer/src/main/java/org/onebusaway/gtfs_transformer/impl/RemoveFlexStrategy.java
+++ onebusaway-gtfs-transformer/src/main/java/org/onebusaway/gtfs_transformer/impl/RemoveFlexStrategy.java
@@ -2,6 +2,7 @@ package org.onebusaway.gtfs_transformer.impl;
 
 import static org.onebusaway.gtfs_transformer.impl.FilterGtfsFlexStrategy.IS_FLEX;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 import org.onebusaway.gtfs.model.Trip;
@@ -36,22 +37,35 @@ public class RemoveFlexStrategy implements GtfsTransformStrategy {
       REMOVE_ENTITY_LIBRARY.removeTrip(dao, trip);
     }
 
-    for (var locationGroupElement : dao.getAllLocationGroupElements()) {
+    var routes = new ArrayList<>(dao.getAllRoutes());
+    for (var route : routes) {
+      var trips = dao.getTripsForRoute(route);
+      if (tripsToRemove.containsAll(trips)) {
+        LOGGER.info("Removing route id {}", route.getId().getId());
+        REMOVE_ENTITY_LIBRARY.removeRoute(dao, route);
+      }
+    }
+
+    var locationGroupElements = new ArrayList<>(dao.getAllLocationGroupElements());
+    for (var locationGroupElement : locationGroupElements) {
       LOGGER.info("Removing location group element id {}", locationGroupElement.getId());
       REMOVE_ENTITY_LIBRARY.removeLocationGroupElement(dao, locationGroupElement);
     }
 
-    for (var location : dao.getAllLocations()) {
+    var locations = new ArrayList<>(dao.getAllLocations());
+    for (var location : locations) {
       LOGGER.info("Removing location id {}", location.getId().getId());
       REMOVE_ENTITY_LIBRARY.removeLocation(dao, location);
     }
 
-    for (var locationGroup : dao.getAllLocationGroups()) {
+    var locationGroups = new ArrayList<>(dao.getAllLocationGroups());
+    for (var locationGroup : locationGroups) {
       LOGGER.info("Removing location group id {}", locationGroup.getId().getId());
       REMOVE_ENTITY_LIBRARY.removeLocationGroup(dao, locationGroup);
     }
 
-    for (var bookingRule : dao.getAllBookingRules()) {
+    var bookingRules = new ArrayList<>(dao.getAllBookingRules());
+    for (var bookingRule : bookingRules) {
       LOGGER.info("Removing booking rule id {}", bookingRule.getId().getId());
       REMOVE_ENTITY_LIBRARY.removeBookingRule(dao, bookingRule);
     }
